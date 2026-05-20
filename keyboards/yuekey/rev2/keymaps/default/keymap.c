75e28ed90c92abdfedd12a8f483d8129fffde824
Yue Huang <hy32000@gmail.com>
2026-05-19 21:31:00 -0400

    update rev2 mapping

--- keyboards/yuekey/rev2/keymaps/default/keymap.c
+++ keyboards/yuekey/rev2/keymaps/default/keymap.c
@@ -22,12 +22,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * '-------------------'
    */
   [_BL] = LAYOUT_numpad_10x5(
-    KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,
+    KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0,
     KC_Q, KC_W, KC_E, KC_R, KC_T,KC_Y, KC_U, KC_I, KC_Q, KC_P,
     KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_ENTER,
     KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_NO, KC_NO, KC_SLASH,
-    KC_LEFT_CTRL, KC_LEFT_SHIFT, KC_CAPS, KC_LEFT_ALT, KC_NO, KC_NO, KC_NO, KC_SPACE, KC_BACKSPACE, KC
-    KC_SPC, KC_CAPS, KC_LCTL, KC_LSFT, KC_L     
+    KC_LEFT_CTRL, KC_LEFT_SHIFT, KC_CAPS, KC_LEFT_ALT, KC_NO, KC_NO, KC_NO, KC_SPACE, KC_BACKSPACE, KC_NO 
   )
 
   /* Keymap _FL: Function Layer
